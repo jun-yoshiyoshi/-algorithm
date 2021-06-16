9603f5fe623e7a2cba791fce0717a514a47fbdeb
jun-yoshiyoshi <80947237+jun-yoshiyoshi@users.noreply.github.com>
2021-06-16 13:18:47 +0900

    Add files via upload

---
+++ bubble_sort2.py
@@ -0,0 +1,11 @@
+import random
+n = int(input())
+data = [random.randint(1, 99) for _ in range(n)]
+print(data, "元のデータ")
+
+for i in range(n - 1):
+    for j in range(n - 1 - i):
+        if data[j] > data[j + 1]:
+            data[j], data[j + 1] = data[j + 1], data[j]
+
+print(data)
